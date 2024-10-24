d0c8ffab396506783e17f0393c91d78466462783
Matheus Lucas <nerdola.cartola@gmail.com>
2024-10-24 00:24:45 -0300

    merge dash

--- game/player/scripts/states/player_state_dash.gd
+++ game/player/scripts/states/player_state_dash.gd
@@ -1,38 +1,59 @@
 class_name Dash extends PlayerState
 
-@export var move_speed: float = 200.0
-@onready var idle: PlayerState = $"../Idle"
-@onready var attack: PlayerState = $"../Attack"
+@onready var dash_cool_down: Timer = $DashCoolDown
+
+@export var move_speed: float
+@export var next_state: PlayerState
+@export var dash_duration: float
+
+var animation_name = "walking"
+var timer: float = 0
+var dash_direction: Vector2
+var enabled: bool = true
+var skip: bool = false
 
 func init():
-	pass
+	dash_cool_down.timeout.connect(on_dash_cool_down)
 
 func enter() -> void:
-	player.update_animation("walking")
-	pass
-
+	if !enabled:
+		skip = true
+		return
+	
+	enabled = false
+	dash_cool_down.start()
+	timer = dash_duration
+	
+	dash_direction = player.direction
+	if dash_direction == Vector2.ZERO:
+		dash_direction = player.cardinal_direction
+		
+	player.make_invulnerable(2*dash_duration)
+	player.update_animation(animation_name)
+	
 func exit() -> void:
 	pass
 	
-func process( _delta: float) -> PlayerState:
-	if player.direction == Vector2.ZERO:
-		return idle
+func process(delta: float) -> PlayerState:
+	if skip:
+		return next_state
 		
+	timer -= delta
+	player.direction = dash_direction
+	
+	if timer <= 0:
+		return next_state
+	
 	player.velocity = move_speed * player.direction
 	
-	if player.set_direction():
-		player.update_animation("walking")
-		
 	return null
 	
 func physics(_delta: float) -> PlayerState:
 	return null
 
-func handle_input(event: InputEvent) -> PlayerState:
-	if event.is_action("attack"):
-		return attack
-		
-	if event.is_action("interact"):
-		GlobalPlayerManager.interact_pressed.emit()
-		
+func handle_input(_event: InputEvent) -> PlayerState:
 	return null
+	
+func on_dash_cool_down():
+	enabled = true
+	skip = false
